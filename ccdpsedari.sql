694fe94a5f31fcbdb6bf3802d3f56956041bf7d2
chiqors <bandi_nada@live.com>
2021-07-28 14:36:07 +0700

    db upload

---
+++ ccdpsedari.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 28, 2021 at 07:35 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ccdpsedari`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `jumlah_beli` int(11) DEFAULT NULL,
+  `total_harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kupon`
+--
+
+CREATE TABLE `kupon` (
+  `id` int(11) NOT NULL,
+  `tanggal_mulai` date NOT NULL,
+  `tanggal_hangus` date DEFAULT NULL,
+  `diskon` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kupon`
+--
+
+INSERT INTO `kupon` (`id`, `tanggal_mulai`, `tanggal_hangus`, `diskon`) VALUES
+(3, '2019-07-17', '2019-07-20', 10),
+(4, '2019-07-18', '2019-07-19', 5),
+(5, '2019-07-17', '2019-07-18', 5),
+(6, '2021-07-26', '2021-08-31', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `nama_menu` varchar(30) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `stok` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `nama_menu`, `harga`, `stok`) VALUES
+(1, 'Soto Betawi', 25000, 100),
+(2, 'Soto Ayam Sunda', 20000, 95),
+(3, 'Soto Boyolali', 15000, 25);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `nip` varchar(10) NOT NULL,
+  `nama` varchar(25) DEFAULT NULL,
+  `alamat` text NOT NULL,
+  `kontak` varchar(15) DEFAULT NULL,
+  `jabatan` varchar(10) DEFAULT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(155) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`nip`, `nama`, `alamat`, `kontak`, `jabatan`, `username`, `password`) VALUES
+('10117053', 'Oktavian Putra', 'Jln Cigantung no 2', '08325345355', 'CEO', 'oktavian', '97ca8ef6b9f855f1e43381fb7b709c20'),
+('10117054', 'Chiqo Rifky Saputra', 'Jln. Berlian no 6', '081223939528', 'Manager', 'chiqors', '3cb3ccfde42856dbfd7a7072a78dbb9e'),
+('10117055', 'Cessario', 'Jl Purwakerto no 7', '081223939544', 'Kasir', 'cessario', '2a7665fa097a22534e5c7f547ae4f752');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `tanggal` datetime DEFAULT NULL,
+  `sub_total` int(11) DEFAULT NULL,
+  `kupon` int(11) DEFAULT NULL,
+  `total_harga` int(11) DEFAULT NULL,
+  `bayar` int(11) DEFAULT NULL,
+  `kembalian` int(11) DEFAULT NULL,
+  `kasir` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_menu` (`id_menu`),
+  ADD KEY `id_transaksi` (`id_transaksi`);
+
+--
+-- Indexes for table `kupon`
+--
+ALTER TABLE `kupon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kasir` (`kasir`),
+  ADD KEY `kupon` (`kupon`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `kupon`
+--
+ALTER TABLE `kupon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`kasir`) REFERENCES `pengguna` (`nip`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`kupon`) REFERENCES `kupon` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
